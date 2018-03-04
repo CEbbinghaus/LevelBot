01ff8d00d283bbb2277be715ec422f0fd3967431
CEbbinghaus <cebbinghaus@live.de>
2018-03-04 23:07:39 +1100

    added Move command

---
+++ commands/move.js
@@ -0,0 +1,24 @@
+const s = require("../settings.json")
+module.exports = {
+	help: {
+		name: "Move",
+		cmd: "mv",
+		use: "mv [Mention of the person] [The level number you want to move to]",
+		extra : "the max level is 10",
+		perms: ["KICK_MEMBERS"]
+	},
+	checkForErrors: function(a){
+		if(parseInt(a[0]) > 10)return "The Max Level is 10";
+		else return this.help.use;
+	},
+	run:function(c, a, m, b){
+		if(!m.member.hasPermission(this.help.perms))return m.react(":p:");
+		let user = m.mentions.first();
+		console.log(a);
+		if(!a || isNaN(parseInt(a[0])) || parseInt(a[0]) > 10)return m.react("❌").then(() => m.react("❓"));
+		let lvs = s.levels;
+		user.member.removeRoles(m.member.roles.filterArray(v => {return lvs[parseInt(v.name.split(" ")[1])]})).then(() => {
+			user.member.addRole(lvs[a[0]]);	
+		})
+	}
+}
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const handleMessage = m => {
 	if(m.author.bot)return;
 	if(!m.content.startsWith(settings.prefix) || !m.content.endsWith(settings.suffix))return;
 	let args = m.content.slice(settings.prefix.length, m.content.length - settings.suffix.length).split(" ");
-	let command = args.shift();
+	let command = args.shift().toLowerCase();
 	try{
 		let cmh = require(`./commands/${command}.js`);
 		cmh.run(command, args, m, Bot);
@@ -31,12 +31,14 @@ Bot.on("messageUpdate", (om, nm) => {
 Bot.on("messageReactionAdd", (r, u) => {
 	if(u.bot)return;
 	if(r.emoji.name == "❓" && r.message.author.id == u.id){
+		let a = r.message.content.slice(settings.prefix.length, r.message.content.length - settings.suffix.length).split(" ");
+		let c = a.shift().toLowerCase();
 		try{
-			let a = r.message.content.slice(settings.prefix.length, r.message.content.length - settings.suffix.length).split(" ");
-			let c = a.shift();
 			let cmh = require(`./commands/${c}.js`);
 			r.message.channel.send(u + "\n" + cmh.checkForErrors(a));
-		}catch(err){}
+		}catch(err){
+			r.message.channel.send(`${u}\nthere is no command with the name ${c}`);
+		}
 	}
 })
 
