51e53d818357d600d13f3e3213c3d674aacdbccb
CEbbinghaus <cebbinghaus@live.de>
2018-03-11 22:55:01 +1100

    Fixed Lv command removing Mod role

--- commands/lv.js
+++ commands/lv.js
@@ -12,10 +12,14 @@ module.exports = {
 		else return this.help.use;
 	},
 	run:function(c, a, m, b){
-		console.log(parseInt(a[0]))
 		if(!a || isNaN(parseInt(a[0])) || parseInt(a[0]) > 10)return m.react("❌").then(() => m.react("❓"));
 		let lvs = s.levels;
-		m.member.removeRoles(m.member.roles.filterArray(v => {return lvs[parseInt(v.name.split(" ")[1])]})).then(() => {
+		//Wlet lv = /\d+/.exec(a.join())[1]
+		let f = m.member.roles.filterArray(v => {
+			let n = /\d+/.exec(v.name);
+			return n == null ? false : (lvs[n[0]] == v.id);
+		});
+		m.member.removeRoles(f).then(() => {
 			m.member.addRole(lvs[a[0]]);	
 		})
 	}
